8204a4b45023006ae7b8d04a538ce3c3a86acbb6
myeongpil <audvlf92@naver.com>
2025-07-20 11:47:33 +0900

    [MAKE] pageComponent & listComponent

--- src/pages/todo/listPage.tsx
+++ src/pages/todo/listPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchParams } from "react-router";
+import ListComponent from "../../components/todo/listComponent";
 
 function listPage() {
     const [queryParams] = useSearchParams()
@@ -8,6 +9,7 @@ function listPage() {
     return ( 
       <div className="bg-white w-full">
           <div className="text-4xl">Todo List Page {page} {size}</div>
+          <ListComponent/>
       </div>
 
      );
