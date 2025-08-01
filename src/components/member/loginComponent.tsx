b467690705917ea7d5a5b2d6cd8fc0f757f533f6
myeongpil <audvlf92@naver.com>
2025-08-02 00:37:33 +0900

    loginPostAsync

--- src/components/member/loginComponent.tsx
+++ src/components/member/loginComponent.tsx
@@ -1,7 +1,8 @@
 
-import { useActionState, useEffect } from "react";
-import { login } from "../../slices/loginSlice";
-import { useDispatch } from "react-redux";
+import { useActionState, useEffect, useState } from "react";;
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../../store";
+import { loginPostAsync } from "../../slices/loginSlice";
 
 interface LoginResult {
  email: string,
@@ -13,54 +14,52 @@ const initState: LoginResult = {
 }
 
 function LoginComponent() {
- const dispatch = useDispatch()   
- const [state, action, isPending] = useActionState(async (state:LoginResult, formData:FormData) => {
-    //2초간 delay
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const email = formData.get('email') as string
-    const pw = formData.get('pw') as string
-    return {email: email, signed:true}
- }, initState)
+    const dispatch = useDispatch<AppDispatch>()
+    const loginStatus = useSelector((state: RootState) => state.loginSlice.status)
+    const [email, setEmail] = useState('')
+    const [pw, setPw] = useState('')
+    const handleLogin = () => {
+    dispatch(loginPostAsync({ email, pw }))
 
- useEffect( () => {
-    if(state.signed) {
-        dispatch(login(state))
     }
- })
+
 
  return (
     <div className = "border-2 border-sky-200 mt-10 m-2 p-4">
-    {isPending && <div>로그인 처리중...</div>}
-    <form action={action}>
-    <div className="flex justify-center">
-        <div className="text-4xl m-4 p-4 font-extrabold text-blue-500">Login Component</div>
-    </div> 
-    <div className="flex justify-center">
-    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-    <div className="w-full p-6 text-left font-bold">Email</div>
-    <input className="w-full p-6 rounded-r border border-solid border-neutral-500 shadow-md" 
-       name="email" type={'text'}/>
-    </div>
-    </div>
-   <div className="flex justify-center">
-    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-     <div className="w-full p-6 text-left font-bold">Password</div>
-     <input className="w-full p-6 rounded-r border border-solid border-neutral-500 shadow-md" 
-        name="pw" type={'password'}/> 
-    </div>
-   </div>
-        <div className="flex justify-center">
-     <div className="relative mb-4 flex w-full justify-center">
-     <div className="w-2/5 p-6 flex justify-center font-bold">
-      <button type='submit' className="rounded p-4 w-36 bg-blue-500 text-xl text-white">
-       LOGIN
-      </button>
-     </div>
-    </div>
-   </div>
-  </form> 
+   {loginStatus === 'pending' && <div className="bg-amber-300">로그인 중...</div>}
+   {loginStatus === 'fulfilled' && <div className="bg-green-300">로그인 성공!</div>}
+   {loginStatus === 'rejected' && <div className="bg-red-300">로그인 실패</div>}
+      <div className="flex justify-center">
+      <div className="text-4xl m-4 p-4 font-extrabold text-blue-500">Login Component</div>
+      </div> 
+      <div className="flex justify-center">
+      <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+       <div className="w-full p-6 text-left font-bold">Email</div>
+       <input className="w-full p-6 rounded-r border border-solid border-neutral-500 shadow-md" 
+            name="email" type={'text'} onChange={(e) => setEmail(e.target.value)}/>
+      </div>
+      </div>
+      <div className="flex justify-center">
+      <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+       <div className="w-full p-6 text-left font-bold">Password</div>
+       <input className="w-full p-6 rounded-r border border-solid border-neutral-500 shadow-md" 
+            name="pw" type={'password'} onChange={(e) => setPw(e.target.value)}/>
+      </div>
+      </div>
+      <div className="flex justify-center">
+      <div className="relative mb-4 flex w-full justify-center">
+       <div className="w-2/5 p-6 flex justify-center font-bold">
+         <button type='submit' 
+         className="rounded p-4 w-36 bg-blue-500 text-xl text-white"
+         onClick={() => handleLogin()}>
+            LOGIN
+         </button>
+       </div>
+      </div>
+      </div> 
  </div>
  )
+
 }
 
 export default LoginComponent
--- src/slices/loginSlice.tsx
+++ src/slices/loginSlice.tsx
@@ -1,27 +1,60 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { loginPost } from "../api/memberApi"
 
-
+//API 서버의 결과를 받도록 변경 
 export interface LoginInfo {
- email:string
+    email:string,
+    nickname:string,
+    accessToken: string, 
+    refreshToken: string,
+    roleNames: string[],
+    status: string
 }
-const initState:LoginInfo = {
- email: ''
+ const initState:LoginInfo = {
+    email: '',
+    nickname: '',
+    accessToken:'',
+    refreshToken: '',
+    roleNames: [],
+    status: ''
 }
+
+export const loginPostAsync = createAsyncThunk('loginPostAsync', ({email, pw}: {email:string, pw:string}) => {
+    console.log("---------------loginPostAsync---------------------")
+    console.log(email, pw)
+    return loginPost(email, pw)
+})
+
+
 
 const loginSlice = createSlice( {
  name: 'LoginSlice',
  initialState: initState,
  reducers: {
-  login: (state, action) => {
-   console.log("login")
-   return {email: action.payload.email}
-  },
-  logout: (state, action) => {
+   logout: (state, action) => {
    console.log("logout")
-   return {email:''}
   }
- }
+ },
+ extraReducers : (builder) =>{
+        builder.addCase( loginPostAsync.fulfilled, (state, action) => { 
+        console.log("fulfilled")
+        const newState = {...action.payload}
+        console.log("payload", action.payload)
+        newState.status = 'fulfilled'
+        
+        return newState
+      })
+      .addCase(loginPostAsync.pending, (state,action) => {
+        console.log("pending")
+        state.status = 'pending'
+      })
+      .addCase(loginPostAsync.rejected, (state,action) => {
+        console.log("rejected")
+        state.status = 'rejected'
+      })
+     }
+
 })
 
-export const {login,logout} = loginSlice.actions
+export const {logout} = loginSlice.actions
 export default loginSlice.reducer
