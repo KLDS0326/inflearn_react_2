3ebc80c06756c0a2e92e8516ca3b1fb1c0e4f53e
myeongpil <audvlf92@naver.com>
2025-08-02 10:11:42 +0900

    login refress auto regenerate(slience refresh

--- src/util/jwtUtil.ts
+++ src/util/jwtUtil.ts
@@ -1,6 +1,18 @@
 import axios, {AxiosError, type AxiosResponse, type InternalAxiosRequestConfig} from "axios";
-import { getCookie } from "./cookieUtil";
+import { getCookie, setCookie } from "./cookieUtil";
 const jwtAxios = axios.create()
+
+const refreshJWT = async (accessToken:string, refreshToken:string) => {
+   const header = {headers: {"Authorization":`Bearer ${accessToken}`}}
+   const res = await axios.get(`http://localhost:8080/api/member/refresh?refreshToken=${refreshToken}`, header)
+  
+   console.log("----------------------")
+   console.log(res.data)
+  
+   return res.data 
+}
+
+
 //before request
 //요청 보내기 전에 추가 작업
 const beforeReq = (config: InternalAxiosRequestConfig) => {
@@ -24,11 +36,28 @@ const requestFail = (err: AxiosError) => {
    console.log("request error............")
    return Promise.reject(err)
 }
-//before return response
+//before return response , slinece refresh
 //성공적인 응답이 왔을 때 추가 작업
 const beforeRes = async (res: AxiosResponse): Promise<AxiosResponse> => {
    console.log("before return response...........")
-   return res
+   const data = res.data 
+   //Access Token이 만료시 
+   if(data && data.error ==='ERROR_ACCESS_TOKEN'){
+    const memberCookieValue = getCookie("member")
+    const result = await refreshJWT( memberCookieValue.accessToken, memberCookieValue.refreshToken )
+    console.log("refreshJWT RESULT", result)
+    
+    memberCookieValue.accessToken = result.accessToken
+    memberCookieValue.refreshToken = result.refreshToken
+    
+    setCookie("member", JSON.stringify(memberCookieValue), 1)
+    
+    //원래의 호출 
+    const originalRequest = res.config
+    originalRequest.headers.Authorization = `Bearer ${result.accessToken}`
+    return await axios(originalRequest)
+    }
+ return res
 }
 //fail response
 const responseFail = async (err: AxiosError) => {
